765d30621408e59c9051e67a05cea09889b12978
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-15 10:50:56 +0300

    Initial Commit

---
+++ Proj-03-BetterRest/Proj_03_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proj_03_BetterRestApp.swift
+//  Proj-03-BetterRest
+//
+//  Created by Андрей Бородкин on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Proj_03_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
